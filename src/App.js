fd010715cad87eca3d4465106adefeb7f90da538
max-sindi <britva.okkamaa@gmail.com>
2018-05-10 11:15:22 +0300

    setuped Basic routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter } from 'react-router-dom'
+
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import { Container, Grid } from 'semantic-ui-react';
+import { Main, CreateContactForm, Page404 } from './components/';
+
 
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
-        <div className="App">
-          <Container>
-            <Grid>
-              <Grid.Column>
-
-              </Grid.Column>
-            </Grid>
-          </Container>
+      <div className="App">
+        <div>
+            <img src={logo} className="App-logo" alt="logo" />
         </div>
-      </BrowserRouter>
+        <BrowserRouter>
+            <Container>
+              <Switch>
+                <Route exact path='/' component={Main} />
+                <Route path='/new' component={CreateContactForm} />
+                <Route component={Page404} />
+              </Switch>
+            </Container>
+        </BrowserRouter>
+      </div>
     );
   }
 }
