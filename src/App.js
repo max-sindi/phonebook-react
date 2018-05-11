a5207b62fb667ea077aafa73872a46031de6846e
max-sindi <britva.okkamaa@gmail.com>
2018-05-11 15:37:21 +0300

    logo as link to main page

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
 
 import { Container, Grid } from 'semantic-ui-react';
 import { Main, CreateContactForm, Page404 } from './components/';
@@ -12,11 +12,13 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div>
-            <img src={logo} className="App-logo" alt="logo" />
-        </div>
         <BrowserRouter>
             <Container>
+              <Link to='/'>
+                <div>
+                    <img src={logo} className="App-logo" alt="logo" />
+                </div>
+              </Link>
               <Switch>
                 <Route exact path='/' component={Main} />
                 <Route path='/new' component={CreateContactForm} />
